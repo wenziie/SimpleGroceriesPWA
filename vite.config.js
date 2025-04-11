9796ead104faade4f51d4c818dfcb8df2f70a124
wenziie <n.sandgren@live.se>
2025-04-11 17:32:07 +0200

    Fix: Add 180x180 icon size to manifest for iOS

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,19 @@ export default defineConfig({
         icons: [
           {
             src: 'icon.png',
-            sizes: '192x192 512x512',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'icon.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'icon.png',
+            sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
           }
